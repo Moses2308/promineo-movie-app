6485e2b4e3e9a2a3f7c54047655058fb671e9168
Moses <moses2308ventura@gmail.com>
2024-03-12 11:51:30 -0700

    last working save

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,18 +1,20 @@
 import getMovieData from "../requests";
-const movie = await getMovieData("Dune");
+const movie = await getMovieData("lorax");
 
 function Movie() {
   return (
     <>
       <article className="movie">
-        <img src={movie.Poster} alt="Movie Poster" />
+        <img className="movie--poster" src={movie.Poster} alt="Movie Poster" />
         <div className="movie__info">
-          <h1 className="movie--title">{movie.Title}</h1>
-          <p className="movie--year">Release Year : {movie.Year}</p>
+          <h2 className="movie--title">{movie.Title}</h2>
           <p className="movie--genre">Genre : {movie.Genre}</p>
+          <p className="movie--year">Release Year : {movie.Year}</p>
           <p className="movie--ageRating">Rated : {movie.Rated}</p>
+          <h3>Synopsis</h3>
+          <p className="movie--synopsis">{movie.Plot}</p>
         </div>
-        <p className="movie--synopsis">{movie.Plot}</p>
+        <div className="movie__rating"></div>
       </article>
     </>
   );
